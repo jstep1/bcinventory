d7b465261ef592d803fce967fb955c4f8af67e03
Jeremy Stepanovich <jstepanovich91@gmail.com>
2019-03-21 18:00:49 -0500

    backed up files

--- app.js
+++ app.js
@@ -1,193 +1,48 @@
-$(".sil5").text(localStorage.getItem("sil5"));
-$(".sil8").text(localStorage.getItem("sil8"));
-$(".sil14").text(localStorage.getItem("sil14"));
-$(".tad3").text(localStorage.getItem("tad3"));
-$(".tad5").text(localStorage.getItem("tad5"));
-$(".tad9").text(localStorage.getItem("tad9"));
+var config = {
+    apiKey: "AIzaSyAT9ZoPFYzrEXij9Oj_LPIiDbWvH_sWGuY",
+    authDomain: "bc-inventory.firebaseapp.com",
+    databaseURL: "https://bc-inventory.firebaseio.com",
+    projectId: "bc-inventory",
+    storageBucket: "bc-inventory.appspot.com",
+    messagingSenderId: "677429783765"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+    
+    var s5bin = ["S5A", "S5B", "S5C", "S5D", "S5E", "S5F", "S5G", "S5H", "S5I", "S5J", "S5K", "S5L", "S5M", "S5N"];
+    var s8bin = ["S8A", "S8B", "S8C", "S8D", "S8E", "S8F", "S8G", "S8H", "S8I", "S8J", "S8K", "S8L", "S8M", "S8N"];
+    var s14bin = ["S14A", "S14B", "S14C", "S14D", "S14E", "S14F", "S14G", "S14H", "S14I", "S14J", "S14K", "S14L", "S14M", "S14N"]; var t3bin = ["T03A", "T03B", "T03C", "T03D", "T03E", "T03F", "T03G", "T03H", "T03I", "T03J", "T03K", "T03L", "T03M", "T03N"]; var t5bin = ["T05A", "T05B", "T05C", "T05D", "T05E", "T05F", "T05G", "T05H", "T05I", "T05J", "T05K", "T05L", "T05M", "T05N"]; var t9bin = ["T09A", "T09B", "T09C", "T09D", "T09E", "T09F", "T09G", "T09H", "T09I", "T09J", "T09K", "T09L", "T09M", "T09N"];
 
-$(".S5A").text(localStorage.getItem("S5A"));
-$(".S5B").text(localStorage.getItem("S5B"));
-$(".S5C").text(localStorage.getItem("S5C"));
-$(".S5D").text(localStorage.getItem("S5D"));
-$(".S5E").text(localStorage.getItem("S5E"));
-$(".S5F").text(localStorage.getItem("S5F"));
-$(".S5G").text(localStorage.getItem("S5G"));
-$(".S5H").text(localStorage.getItem("S5H"));
-$(".S5I").text(localStorage.getItem("S5I"));
-$(".S5J").text(localStorage.getItem("S5J"));
-$(".S5K").text(localStorage.getItem("S5K"));
-$(".S5L").text(localStorage.getItem("S5L"));
-$(".S5M").text(localStorage.getItem("S5M"));
-$(".S5N").text(localStorage.getItem("S5N"));
+    var allbins = [];
 
-$(".S8A").text(localStorage.getItem("S8A"));
-$(".S8B").text(localStorage.getItem("S8B"));
-$(".S8C").text(localStorage.getItem("S8C"));
-$(".S8D").text(localStorage.getItem("S8D"));
-$(".S8E").text(localStorage.getItem("S8E"));
-$(".S8F").text(localStorage.getItem("S8F"));
-$(".S8G").text(localStorage.getItem("S8G"));
-$(".S8H").text(localStorage.getItem("S8H"));
-$(".S8I").text(localStorage.getItem("S8I"));
-$(".S8J").text(localStorage.getItem("S8J"));
-$(".S8K").text(localStorage.getItem("S8K"));
-$(".S8L").text(localStorage.getItem("S8L"));
-$(".S8M").text(localStorage.getItem("S8M"));
-$(".S8N").text(localStorage.getItem("S8N"));
+    var invtotals = [];
+    var sil14total, sil5total, sil8total, tad3total, tad5total, tad9total;
+    
+    database.ref("/totals").once("value", function(inval) {
+        inval.forEach(function (invnumb) {
+            invtotals.push(invnumb.val());
+        })}).then(function () {
+            $(".sil5").text(invtotals[1]);
+            $(".sil8").text(invtotals[2]);
+            $(".sil14").text(invtotals[0]);
+            $(".tad3").text(invtotals[3]);
+            $(".tad5").text(invtotals[4]);
+            $(".tad9").text(invtotals[5]);
+        });
 
-$(".S14A").text(localStorage.getItem("S14A"));
-$(".S14B").text(localStorage.getItem("S14B"));
-$(".S14C").text(localStorage.getItem("S14C"));
-$(".S14D").text(localStorage.getItem("S14D"));
-$(".S14E").text(localStorage.getItem("S14E"));
-$(".S14F").text(localStorage.getItem("S14F"));
-$(".S14G").text(localStorage.getItem("S14G"));
-$(".S14H").text(localStorage.getItem("S14H"));
-$(".S14I").text(localStorage.getItem("S14I"));
-$(".S14J").text(localStorage.getItem("S14J"));
-$(".S14K").text(localStorage.getItem("S14K"));
-$(".S14L").text(localStorage.getItem("S14L"));
-$(".S14M").text(localStorage.getItem("S14M"));
-$(".S14N").text(localStorage.getItem("S14N"));
+    database.ref("/bins").once("value", function(bnum){
+        bnum.forEach(function (bnqty) {
+            allbins.push(bnqty.val());
+        })}).then(function () {
+            var idnum = 0;
+            for(i = 0; i < 6; i++) {
+                for(j = 0; j < 14; j++) {
+                $("#i" + idnum).text(allbins[i][Object.keys(allbins[i])[j]].quantity)
+                idnum++;
+                }
+            }
+        })
 
-$(".T03A").text(localStorage.getItem("T03A"));
-$(".T03B").text(localStorage.getItem("T03B"));
-$(".T03C").text(localStorage.getItem("T03C"));
-$(".T03D").text(localStorage.getItem("T03D"));
-$(".T03E").text(localStorage.getItem("T03E"));
-$(".T03F").text(localStorage.getItem("T03F"));
-$(".T03G").text(localStorage.getItem("T03G"));
-$(".T03H").text(localStorage.getItem("T03H"));
-$(".T03I").text(localStorage.getItem("T03I"));
-$(".T03J").text(localStorage.getItem("T03J"));
-$(".T03K").text(localStorage.getItem("T03K"));
-$(".T03L").text(localStorage.getItem("T03L"));
-$(".T03M").text(localStorage.getItem("T03M"));
-$(".T03N").text(localStorage.getItem("T03N"));
-
-$(".T05A").text(localStorage.getItem("T05A"));
-$(".T05B").text(localStorage.getItem("T05B"));
-$(".T05C").text(localStorage.getItem("T05C"));
-$(".T05D").text(localStorage.getItem("T05D"));
-$(".T05E").text(localStorage.getItem("T05E"));
-$(".T05F").text(localStorage.getItem("T05F"));
-$(".T05G").text(localStorage.getItem("T05G"));
-$(".T05H").text(localStorage.getItem("T05H"));
-$(".T05I").text(localStorage.getItem("T05I"));
-$(".T05J").text(localStorage.getItem("T05J"));
-$(".T05K").text(localStorage.getItem("T05K"));
-$(".T05L").text(localStorage.getItem("T05L"));
-$(".T05M").text(localStorage.getItem("T05M"));
-$(".T05N").text(localStorage.getItem("T05N"));
-
-$(".T09A").text(localStorage.getItem("T09A"));
-$(".T09B").text(localStorage.getItem("T09B"));
-$(".T09C").text(localStorage.getItem("T09C"));
-$(".T09D").text(localStorage.getItem("T09D"));
-$(".T09E").text(localStorage.getItem("T09E"));
-$(".T09F").text(localStorage.getItem("T09F"));
-$(".T09G").text(localStorage.getItem("T09G"));
-$(".T09H").text(localStorage.getItem("T09H"));
-$(".T09I").text(localStorage.getItem("T09I"));
-$(".T09J").text(localStorage.getItem("T09J"));
-$(".T09K").text(localStorage.getItem("T09K"));
-$(".T09L").text(localStorage.getItem("T09L"));
-$(".T09M").text(localStorage.getItem("T09M"));
-$(".T09N").text(localStorage.getItem("T09N"));
-
-var S5total = parseInt(localStorage.getItem("S5A"))
-+ parseInt(localStorage.getItem("S5B"))
-+ parseInt(localStorage.getItem("S5C"))
-+ parseInt(localStorage.getItem("S5D"))
-+ parseInt(localStorage.getItem("S5E"))
-+ parseInt(localStorage.getItem("S5F"))
-+ parseInt(localStorage.getItem("S5G"))
-+ parseInt(localStorage.getItem("S5H"))
-+ parseInt(localStorage.getItem("S5I"))
-+ parseInt(localStorage.getItem("S5J"))
-+ parseInt(localStorage.getItem("S5K"))
-+ parseInt(localStorage.getItem("S5L"))
-+ parseInt(localStorage.getItem("S5M"))
-+ parseInt(localStorage.getItem("S5N"));
-
-var S8total = parseInt(localStorage.getItem("S8A"))
-+ parseInt(localStorage.getItem("S8B"))
-+ parseInt(localStorage.getItem("S8C"))
-+ parseInt(localStorage.getItem("S8D"))
-+ parseInt(localStorage.getItem("S8E"))
-+ parseInt(localStorage.getItem("S8F"))
-+ parseInt(localStorage.getItem("S8G"))
-+ parseInt(localStorage.getItem("S8H"))
-+ parseInt(localStorage.getItem("S8I"))
-+ parseInt(localStorage.getItem("S8J"))
-+ parseInt(localStorage.getItem("S8K"))
-+ parseInt(localStorage.getItem("S8L"))
-+ parseInt(localStorage.getItem("S8M"))
-+ parseInt(localStorage.getItem("S8N"));
-
-var S14total = parseInt(localStorage.getItem("S14A"))
-+ parseInt(localStorage.getItem("S14B"))
-+ parseInt(localStorage.getItem("S14C"))
-+ parseInt(localStorage.getItem("S14D"))
-+ parseInt(localStorage.getItem("S14E"))
-+ parseInt(localStorage.getItem("S14F"))
-+ parseInt(localStorage.getItem("S14G"))
-+ parseInt(localStorage.getItem("S14H"))
-+ parseInt(localStorage.getItem("S14I"))
-+ parseInt(localStorage.getItem("S14J"))
-+ parseInt(localStorage.getItem("S14K"))
-+ parseInt(localStorage.getItem("S14L"))
-+ parseInt(localStorage.getItem("S14M"))
-+ parseInt(localStorage.getItem("S14N"));
-
-var T03total = parseInt(localStorage.getItem("T03A"))
-+ parseInt(localStorage.getItem("T03B"))
-+ parseInt(localStorage.getItem("T03C"))
-+ parseInt(localStorage.getItem("T03D"))
-+ parseInt(localStorage.getItem("T03E"))
-+ parseInt(localStorage.getItem("T03F"))
-+ parseInt(localStorage.getItem("T03G"))
-+ parseInt(localStorage.getItem("T03H"))
-+ parseInt(localStorage.getItem("T03I"))
-+ parseInt(localStorage.getItem("T03J"))
-+ parseInt(localStorage.getItem("T03K"))
-+ parseInt(localStorage.getItem("T03L"))
-+ parseInt(localStorage.getItem("T03M"))
-+ parseInt(localStorage.getItem("T03N"));
-
-var T05total = parseInt(localStorage.getItem("T05A"))
-+ parseInt(localStorage.getItem("T05B"))
-+ parseInt(localStorage.getItem("T05C"))
-+ parseInt(localStorage.getItem("T05D"))
-+ parseInt(localStorage.getItem("T05E"))
-+ parseInt(localStorage.getItem("T05F"))
-+ parseInt(localStorage.getItem("T05G"))
-+ parseInt(localStorage.getItem("T05H"))
-+ parseInt(localStorage.getItem("T05I"))
-+ parseInt(localStorage.getItem("T05J"))
-+ parseInt(localStorage.getItem("T05K"))
-+ parseInt(localStorage.getItem("T05L"))
-+ parseInt(localStorage.getItem("T05M"))
-+ parseInt(localStorage.getItem("T05N"));
-
-var T09total = parseInt(localStorage.getItem("T09A"))
-+ parseInt(localStorage.getItem("T09B"))
-+ parseInt(localStorage.getItem("T09C"))
-+ parseInt(localStorage.getItem("T09D"))
-+ parseInt(localStorage.getItem("T09E"))
-+ parseInt(localStorage.getItem("T09F"))
-+ parseInt(localStorage.getItem("T09G"))
-+ parseInt(localStorage.getItem("T09H"))
-+ parseInt(localStorage.getItem("T09I"))
-+ parseInt(localStorage.getItem("T09J"))
-+ parseInt(localStorage.getItem("T09K"))
-+ parseInt(localStorage.getItem("T09L"))
-+ parseInt(localStorage.getItem("T09M"))
-+ parseInt(localStorage.getItem("T09N"));
-
-$(document).ready(function() {
-    $(".sil5").text(localStorage.setItem("sil5", S5total))
-})
 
 $(".scan").change(function() {
     var s5 = parseInt(localStorage.getItem("sil5"));
@@ -222,6 +77,10 @@ $(".scan").change(function() {
     }
 })
 
+$(".invup").click(function () {
+    alert("Inventory Updated Successfully!");
+})
+
 $(".sil5complete").click(function() {
     var snew = localStorage.getItem(this.id);
     localStorage.setItem(this.id, 0);
@@ -254,3 +113,25 @@ $("#reminv").click(function() {
 })
     
    
+
+
+// function timeSince(date) {
+//   var seconds = Math.floor(new Date().getTime() / 1000 - date),
+//   interval = Math.floor(seconds / 31536000);
+
+// if (interval > 1) return interval + "y";
+
+// interval = Math.floor(seconds / 2592000);
+// if (interval > 1) return interval + "m";
+
+// interval = Math.floor(seconds / 86400);
+// if (interval >= 1) return interval + "d";
+
+// interval = Math.floor(seconds / 3600);
+// if (interval >= 1) return interval + "h";
+
+// interval = Math.floor(seconds / 60);
+// if (interval > 1) return interval + "m ";
+
+// return Math.floor(seconds) + "s";
+// }
