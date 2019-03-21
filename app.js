2bcb24c77f2b2ca5e2af3f415e7cdc92d1fc761f
Jeremy Stepanovich <jstepanovich91@gmail.com>
2019-03-20 19:29:51 -0500

    updated with bin numbers

--- app.js
+++ app.js
@@ -3,131 +3,191 @@ $(".sil8").text(localStorage.getItem("sil8"));
 $(".sil14").text(localStorage.getItem("sil14"));
 $(".tad3").text(localStorage.getItem("tad3"));
 $(".tad5").text(localStorage.getItem("tad5"));
-$(".tad8").text(localStorage.getItem("tad8"));
+$(".tad9").text(localStorage.getItem("tad9"));
 
-$(".sil5add").click(function() {
-    var s = parseInt($(".sil5").text());
-    $(".sil5").text(s+1);
-    var z = $(".sil5").text();
-    localStorage.setItem("sil5", z);
-}); 
+$(".S5A").text(localStorage.getItem("S5A"));
+$(".S5B").text(localStorage.getItem("S5B"));
+$(".S5C").text(localStorage.getItem("S5C"));
+$(".S5D").text(localStorage.getItem("S5D"));
+$(".S5E").text(localStorage.getItem("S5E"));
+$(".S5F").text(localStorage.getItem("S5F"));
+$(".S5G").text(localStorage.getItem("S5G"));
+$(".S5H").text(localStorage.getItem("S5H"));
+$(".S5I").text(localStorage.getItem("S5I"));
+$(".S5J").text(localStorage.getItem("S5J"));
+$(".S5K").text(localStorage.getItem("S5K"));
+$(".S5L").text(localStorage.getItem("S5L"));
+$(".S5M").text(localStorage.getItem("S5M"));
+$(".S5N").text(localStorage.getItem("S5N"));
 
-$(".sil5sub").click(function() {
-    var s = parseInt($(".sil5").text());
-    $(".sil5").text(s-1);
-    var z = $(".sil5").text();
-    localStorage.setItem("sil5", z);
-}); 
+$(".S8A").text(localStorage.getItem("S8A"));
+$(".S8B").text(localStorage.getItem("S8B"));
+$(".S8C").text(localStorage.getItem("S8C"));
+$(".S8D").text(localStorage.getItem("S8D"));
+$(".S8E").text(localStorage.getItem("S8E"));
+$(".S8F").text(localStorage.getItem("S8F"));
+$(".S8G").text(localStorage.getItem("S8G"));
+$(".S8H").text(localStorage.getItem("S8H"));
+$(".S8I").text(localStorage.getItem("S8I"));
+$(".S8J").text(localStorage.getItem("S8J"));
+$(".S8K").text(localStorage.getItem("S8K"));
+$(".S8L").text(localStorage.getItem("S8L"));
+$(".S8M").text(localStorage.getItem("S8M"));
+$(".S8N").text(localStorage.getItem("S8N"));
 
-$(".sil8add").click(function() {
-    var s = parseInt($(".sil8").text());
-    $(".sil8").text(s+1);
-    var z = $(".sil8").text();
-    localStorage.setItem("sil8", z);
-});
+$(".S14A").text(localStorage.getItem("S14A"));
+$(".S14B").text(localStorage.getItem("S14B"));
+$(".S14C").text(localStorage.getItem("S14C"));
+$(".S14D").text(localStorage.getItem("S14D"));
+$(".S14E").text(localStorage.getItem("S14E"));
+$(".S14F").text(localStorage.getItem("S14F"));
+$(".S14G").text(localStorage.getItem("S14G"));
+$(".S14H").text(localStorage.getItem("S14H"));
+$(".S14I").text(localStorage.getItem("S14I"));
+$(".S14J").text(localStorage.getItem("S14J"));
+$(".S14K").text(localStorage.getItem("S14K"));
+$(".S14L").text(localStorage.getItem("S14L"));
+$(".S14M").text(localStorage.getItem("S14M"));
+$(".S14N").text(localStorage.getItem("S14N"));
 
-$(".sil8sub").click(function() {
-    var s = parseInt($(".sil8").text());
-    $(".sil8").text(s-1);
-    var z = $(".sil8").text();
-    localStorage.setItem("sil8", z);
-}); 
+$(".T03A").text(localStorage.getItem("T03A"));
+$(".T03B").text(localStorage.getItem("T03B"));
+$(".T03C").text(localStorage.getItem("T03C"));
+$(".T03D").text(localStorage.getItem("T03D"));
+$(".T03E").text(localStorage.getItem("T03E"));
+$(".T03F").text(localStorage.getItem("T03F"));
+$(".T03G").text(localStorage.getItem("T03G"));
+$(".T03H").text(localStorage.getItem("T03H"));
+$(".T03I").text(localStorage.getItem("T03I"));
+$(".T03J").text(localStorage.getItem("T03J"));
+$(".T03K").text(localStorage.getItem("T03K"));
+$(".T03L").text(localStorage.getItem("T03L"));
+$(".T03M").text(localStorage.getItem("T03M"));
+$(".T03N").text(localStorage.getItem("T03N"));
 
-$(".sil14add").click(function() {
-    var s = parseInt($(".sil14").text());
-    $(".sil14").text(s+1);
-    var z = $(".sil14").text();
-    localStorage.setItem("sil14", z);
-});
+$(".T05A").text(localStorage.getItem("T05A"));
+$(".T05B").text(localStorage.getItem("T05B"));
+$(".T05C").text(localStorage.getItem("T05C"));
+$(".T05D").text(localStorage.getItem("T05D"));
+$(".T05E").text(localStorage.getItem("T05E"));
+$(".T05F").text(localStorage.getItem("T05F"));
+$(".T05G").text(localStorage.getItem("T05G"));
+$(".T05H").text(localStorage.getItem("T05H"));
+$(".T05I").text(localStorage.getItem("T05I"));
+$(".T05J").text(localStorage.getItem("T05J"));
+$(".T05K").text(localStorage.getItem("T05K"));
+$(".T05L").text(localStorage.getItem("T05L"));
+$(".T05M").text(localStorage.getItem("T05M"));
+$(".T05N").text(localStorage.getItem("T05N"));
 
-$(".sil14sub").click(function() {
-    var s = parseInt($(".sil14").text());
-    $(".sil14").text(s-1);
-    var z = $(".sil14").text();
-    localStorage.setItem("sil14", z);
-}); 
+$(".T09A").text(localStorage.getItem("T09A"));
+$(".T09B").text(localStorage.getItem("T09B"));
+$(".T09C").text(localStorage.getItem("T09C"));
+$(".T09D").text(localStorage.getItem("T09D"));
+$(".T09E").text(localStorage.getItem("T09E"));
+$(".T09F").text(localStorage.getItem("T09F"));
+$(".T09G").text(localStorage.getItem("T09G"));
+$(".T09H").text(localStorage.getItem("T09H"));
+$(".T09I").text(localStorage.getItem("T09I"));
+$(".T09J").text(localStorage.getItem("T09J"));
+$(".T09K").text(localStorage.getItem("T09K"));
+$(".T09L").text(localStorage.getItem("T09L"));
+$(".T09M").text(localStorage.getItem("T09M"));
+$(".T09N").text(localStorage.getItem("T09N"));
 
-$(".tad3add").click(function() {
-    var s = parseInt($(".tad3").text());
-    $(".tad3").text(s+1);
-    var z = $(".tad3").text();
-    localStorage.setItem("tad3", z);
-}); 
+var S5total = parseInt(localStorage.getItem("S5A"))
++ parseInt(localStorage.getItem("S5B"))
++ parseInt(localStorage.getItem("S5C"))
++ parseInt(localStorage.getItem("S5D"))
++ parseInt(localStorage.getItem("S5E"))
++ parseInt(localStorage.getItem("S5F"))
++ parseInt(localStorage.getItem("S5G"))
++ parseInt(localStorage.getItem("S5H"))
++ parseInt(localStorage.getItem("S5I"))
++ parseInt(localStorage.getItem("S5J"))
++ parseInt(localStorage.getItem("S5K"))
++ parseInt(localStorage.getItem("S5L"))
++ parseInt(localStorage.getItem("S5M"))
++ parseInt(localStorage.getItem("S5N"));
 
-$(".tad3sub").click(function() {
-    var s = parseInt($(".tad3").text());
-    $(".tad3").text(s-1);
-    var z = $(".tad3").text();
-    localStorage.setItem("tad3", z);
-}); 
+var S8total = parseInt(localStorage.getItem("S8A"))
++ parseInt(localStorage.getItem("S8B"))
++ parseInt(localStorage.getItem("S8C"))
++ parseInt(localStorage.getItem("S8D"))
++ parseInt(localStorage.getItem("S8E"))
++ parseInt(localStorage.getItem("S8F"))
++ parseInt(localStorage.getItem("S8G"))
++ parseInt(localStorage.getItem("S8H"))
++ parseInt(localStorage.getItem("S8I"))
++ parseInt(localStorage.getItem("S8J"))
++ parseInt(localStorage.getItem("S8K"))
++ parseInt(localStorage.getItem("S8L"))
++ parseInt(localStorage.getItem("S8M"))
++ parseInt(localStorage.getItem("S8N"));
 
-$(".tad5add").click(function() {
-    var s = parseInt($(".tad5").text());
-    $(".tad5").text(s+1);
-    var z = $(".tad5").text();
-    localStorage.setItem("tad5", z);
-}); 
+var S14total = parseInt(localStorage.getItem("S14A"))
++ parseInt(localStorage.getItem("S14B"))
++ parseInt(localStorage.getItem("S14C"))
++ parseInt(localStorage.getItem("S14D"))
++ parseInt(localStorage.getItem("S14E"))
++ parseInt(localStorage.getItem("S14F"))
++ parseInt(localStorage.getItem("S14G"))
++ parseInt(localStorage.getItem("S14H"))
++ parseInt(localStorage.getItem("S14I"))
++ parseInt(localStorage.getItem("S14J"))
++ parseInt(localStorage.getItem("S14K"))
++ parseInt(localStorage.getItem("S14L"))
++ parseInt(localStorage.getItem("S14M"))
++ parseInt(localStorage.getItem("S14N"));
 
-$(".tad5sub").click(function() {
-    var s = parseInt($(".tad5").text());
-    $(".tad5").text(s-1);
-    var z = $(".tad5").text();
-    localStorage.setItem("tad5", z);
-}); 
+var T03total = parseInt(localStorage.getItem("T03A"))
++ parseInt(localStorage.getItem("T03B"))
++ parseInt(localStorage.getItem("T03C"))
++ parseInt(localStorage.getItem("T03D"))
++ parseInt(localStorage.getItem("T03E"))
++ parseInt(localStorage.getItem("T03F"))
++ parseInt(localStorage.getItem("T03G"))
++ parseInt(localStorage.getItem("T03H"))
++ parseInt(localStorage.getItem("T03I"))
++ parseInt(localStorage.getItem("T03J"))
++ parseInt(localStorage.getItem("T03K"))
++ parseInt(localStorage.getItem("T03L"))
++ parseInt(localStorage.getItem("T03M"))
++ parseInt(localStorage.getItem("T03N"));
 
-$(".tad8add").click(function() {
-    var s = parseInt($(".tad8").text());
-    $(".tad8").text(s+1);
-    var z = $(".tad8").text();
-    localStorage.setItem("tad8", z);
-}); 
+var T05total = parseInt(localStorage.getItem("T05A"))
++ parseInt(localStorage.getItem("T05B"))
++ parseInt(localStorage.getItem("T05C"))
++ parseInt(localStorage.getItem("T05D"))
++ parseInt(localStorage.getItem("T05E"))
++ parseInt(localStorage.getItem("T05F"))
++ parseInt(localStorage.getItem("T05G"))
++ parseInt(localStorage.getItem("T05H"))
++ parseInt(localStorage.getItem("T05I"))
++ parseInt(localStorage.getItem("T05J"))
++ parseInt(localStorage.getItem("T05K"))
++ parseInt(localStorage.getItem("T05L"))
++ parseInt(localStorage.getItem("T05M"))
++ parseInt(localStorage.getItem("T05N"));
 
-$(".tad8sub").click(function() {
-    var s = parseInt($(".tad8").text());
-    $(".tad8").text(s-1);
-    var z = $(".tad8").text();
-    localStorage.setItem("tad8", z);
-}); 
+var T09total = parseInt(localStorage.getItem("T09A"))
++ parseInt(localStorage.getItem("T09B"))
++ parseInt(localStorage.getItem("T09C"))
++ parseInt(localStorage.getItem("T09D"))
++ parseInt(localStorage.getItem("T09E"))
++ parseInt(localStorage.getItem("T09F"))
++ parseInt(localStorage.getItem("T09G"))
++ parseInt(localStorage.getItem("T09H"))
++ parseInt(localStorage.getItem("T09I"))
++ parseInt(localStorage.getItem("T09J"))
++ parseInt(localStorage.getItem("T09K"))
++ parseInt(localStorage.getItem("T09L"))
++ parseInt(localStorage.getItem("T09M"))
++ parseInt(localStorage.getItem("T09N"));
 
-/////////////
-
-$(".s5up").click(function() {
-    var x = parseInt($(".sil5up").val());
-    $(".sil5").text(x);
-    localStorage.setItem("sil5", x);
+$(document).ready(function() {
+    $(".sil5").text(localStorage.setItem("sil5", S5total))
 })
-
-$(".s8up").click(function() {
-    var x = parseInt($(".sil8up").val());
-    $(".sil8").text(x);
-    localStorage.setItem("sil8", x);
-})
-
-$(".s14up").click(function() {
-    var x = parseInt($(".sil14up").val());
-    $(".sil14").text(x);
-    localStorage.setItem("sil14", x);
-})
-
-$(".t3up").click(function() {
-    var x = parseInt($(".tad3up").val());
-    $(".tad3").text(x);
-    localStorage.setItem("tad3", x);
-})
-
-$(".t5up").click(function() {
-    var x = $(".tad5up").val();
-    $(".tad5").text(x);
-    localStorage.setItem("tad5", x);
-})
-
-$(".t8up").click(function() {
-    var x = parseInt($(".tad8up").val());
-    $(".tad8").text(x);
-    localStorage.setItem("tad8", x);
-})
-
-///////
 
 $(".scan").change(function() {
     var s5 = parseInt(localStorage.getItem("sil5"));
@@ -135,25 +195,36 @@ $(".scan").change(function() {
     var s14 = parseInt(localStorage.getItem("sil14"));
     var t3 = parseInt(localStorage.getItem("tad3"));
     var t5 = parseInt(localStorage.getItem("tad5"));
-    var t8 = parseInt(localStorage.getItem("tad8"));
+    var t9 = parseInt(localStorage.getItem("tad9"));
+
     var x = $(".scan").val();
     
-    if (x === "SIL_5") {
-        localStorage.setItem("sil5", s5 + 1)
+    if (x.includes("S5")) {
+        localStorage.setItem("sil5", S5total)
     }
-    if (x === "SIL_8") {
-        localStorage.setItem("sil8", s8 + 1)
+    if (x.includes("S8")) {
+        localStorage.setItem("sil8", S8total)
     }
-    if (x === "SIL_14") {
-        localStorage.setItem("sil14", s14 + 1)
+    if (x.includes("S14")) {
+        localStorage.setItem("sil14", S14total)
     }
-    if (x === "TAD_03") {
-        localStorage.setItem("tad3", t3 + 1)
+    if (x.includes("T03")) {
+        var t = parseInt(localStorage.getItem(x));
+        localStorage.setItem(x, T03total + t)
     }
-    if (x === "TAD_05") {
-        localStorage.setItem("tad5", t5 + 1)
+    if (x.includes("T05")) {
+        var t = parseInt(localStorage.getItem(x));
+        localStorage.setItem(x, T05total + t)
     }
-    if (x === "TAD_08") {
-        localStorage.setItem("tad8", t8 + 1)
+    if (x.includes("T09")) {
+        var t = parseInt(localStorage.getItem(x));
+        localStorage.setItem(x, T09total + t)
     }
+})
+
+$(".sil5complete").click(function() {
+    var snew = localStorage.getItem(this.id);
+    localStorage.setItem(this.id, 0);
+    localStorage.setItem("sil5", S5total - snew);
+    
 })
