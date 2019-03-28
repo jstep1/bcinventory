18402dda65a8861c45d5c5e4b9d954fc2354e15e
Jeremy Stepanovich <jstepanovich91@gmail.com>
2019-03-27 20:52:37 -0500

    updated bin numbers

--- app.js
+++ app.js
@@ -13,35 +13,56 @@ var config = {
     var s8bin = ["S8A", "S8B", "S8C", "S8D", "S8E", "S8F", "S8G", "S8H", "S8I", "S8J", "S8K", "S8L", "S8M", "S8N"];
     var s14bin = ["S14A", "S14B", "S14C", "S14D", "S14E", "S14F", "S14G", "S14H", "S14I", "S14J", "S14K", "S14L", "S14M", "S14N"]; var t3bin = ["T03A", "T03B", "T03C", "T03D", "T03E", "T03F", "T03G", "T03H", "T03I", "T03J", "T03K", "T03L", "T03M", "T03N"]; var t5bin = ["T05A", "T05B", "T05C", "T05D", "T05E", "T05F", "T05G", "T05H", "T05I", "T05J", "T05K", "T05L", "T05M", "T05N"]; var t9bin = ["T09A", "T09B", "T09C", "T09D", "T09E", "T09F", "T09G", "T09H", "T09I", "T09J", "T09K", "T09L", "T09M", "T09N"];
 
+    var bincollection = ["S5", "S8", "S14", "T03", "T05", "T09"];
+
     var allbins = [];
 
     var invtotals = [];
-    var sil14total, sil5total, sil8total, tad3total, tad5total, tad9total;
-    
-    database.ref("/totals").once("value", function(inval) {
-        inval.forEach(function (invnumb) {
-            invtotals.push(invnumb.val());
-        })}).then(function () {
-            $(".sil5").text(invtotals[1]);
-            $(".sil8").text(invtotals[2]);
-            $(".sil14").text(invtotals[0]);
-            $(".tad3").text(invtotals[3]);
-            $(".tad5").text(invtotals[4]);
-            $(".tad9").text(invtotals[5]);
-        });
 
-    database.ref("/bins").once("value", function(bnum){
-        bnum.forEach(function (bnqty) {
-            allbins.push(bnqty.val());
-        })}).then(function () {
-            var idnum = 0;
-            for(i = 0; i < 6; i++) {
-                for(j = 0; j < 14; j++) {
-                $("#i" + idnum).text(allbins[i][Object.keys(allbins[i])[j]].quantity)
-                idnum++;
-                }
-            }
-        })
+    var binquan = 0;
+
+    var sil14total = 0;
+    var sil5total = 0;
+    var sil8total = 0;
+    var tad3total = 0;
+    var tad5total = 0;
+    var tad9total = 0;
+ 
+    database.ref("/bins").once("value", function(bnum) {
+            allbins.push(bnum.val())}).then(function(invnums) {
+                invnums.forEach(function (xyz) {
+                    xyz.forEach(function (abc) {
+                        var idnum = "#" + abc.val().id;
+                        $(idnum).text(abc.val().qty)
+                    
+                        })})}).then(function() {
+                            for(i=0; i<14; i++) {
+                                sil14total += parseInt($("#i" + i).text());
+                                $(".sil14").text(sil14total)
+                            }
+                            for(i=14; i < 28; i++ ) {
+                                sil5total += parseInt($("#i" + i).text());
+                                $(".sil5").text(sil5total)
+                            }
+                            for(i=28; i < 42; i++ ) {
+                                sil8total += parseInt($("#i" + i).text());
+                                $(".sil8").text(sil8total)
+                            }
+                            for(i=42; i < 56; i++ ) {
+                                tad3total += parseInt($("#i" + i).text());
+                                $(".tad3").text(tad3total)
+                            }
+                            for(i=56; i < 70; i++ ) {
+                                tad5total += parseInt($("#i" + i).text());
+                                $(".tad5").text(tad5total)
+                            }
+                            for(i=70; i < 84; i++ ) {
+                                tad9total += parseInt($("#i" + i).text());
+                                $(".tad9").text(tad9total)
+                            }
+                        })
+
+
 
 
 $(".scan").change(function() {
@@ -111,8 +132,31 @@ $("#reminv").click(function() {
     }
     
 })
+
+// Check in bin functions
+
+$('#checkin').submit(function () {
+    event.preventDefault();
+    var binid = $(".checkin").val()
+    $('.binid').text(binid)
+    $('#checkinbin').modal('show');
+ });
+
+ $('#checkout').submit(function () {
+    event.preventDefault();
+    var binid = $(".checkout").val()
+    $('.binid').text(binid)
+    $('#checkoutbin').modal('show');
+ });
     
-   
+ $("#qtysubmit").click(function() {
+     console.log(database.ref("/bins"))
+     var bnumber = $("#binqty").val();
+     if(bnumber > 0) {
+        alert("Added " + bnumber + " pouches.")
+     }
+     
+ })  
 
 
 // function timeSince(date) {
